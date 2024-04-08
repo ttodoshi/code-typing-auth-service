552d4580bf723e42beecb435a5549d30d0f765d2
todoshi <ttodoshi@users.noreply.github.com>
2024-04-08 16:17:39 +0300

    feat: added index to clear expired tokens

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -5,6 +5,7 @@ import (
 	"code-typing-auth-service/internal/adapters/handler"
 	"code-typing-auth-service/internal/adapters/mq/rabbitmq"
 	"code-typing-auth-service/internal/adapters/repository/mongodb"
+	"code-typing-auth-service/internal/core/domain"
 	"code-typing-auth-service/internal/core/servises"
 	"code-typing-auth-service/pkg/broker"
 	"code-typing-auth-service/pkg/discovery"
@@ -13,8 +14,11 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/kamva/mgm/v3"
 	amqp "github.com/rabbitmq/amqp091-go"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"os"
+	"strconv"
 )
 
 const (
@@ -52,8 +56,26 @@ func main() {
 
 func initDatabase(log logging.Logger) {
 	err := mgm.SetDefaultConfig(nil, "auth", options.Client().ApplyURI(os.Getenv("DB_URL")))
+
 	if err != nil {
 		log.Fatal("failed connect to database")
+	}
+
+	collection := mgm.CollectionByName(
+		mgm.CollName(&domain.RefreshToken{}),
+	)
+	refreshTokenExp, err := strconv.Atoi(os.Getenv("REFRESH_TOKEN_EXP"))
+	if err != nil {
+		log.Fatal("failed to parse refresh token expiration")
+	}
+	refreshTokenExpirationIndex := mongo.IndexModel{
+		Keys:    bson.D{{"updated_at", 1}},
+		Options: options.Index().SetExpireAfterSeconds(int32(refreshTokenExp)),
+	}
+	_, err = collection.Indexes().CreateOne(mgm.Ctx(), refreshTokenExpirationIndex)
+
+	if err != nil {
+		log.Fatal(err.Error())
 	}
 }
 
